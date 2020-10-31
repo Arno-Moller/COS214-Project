2b7efc4c5a5dbce893533991a907c8e17030dee7
TheoMorkel <theo.jnr.morkel@gmail.com>
2020-10-31 15:29:26 +0200

    Update Strategy.h

---
+++ Strategy/Strategy.h
@@ -0,0 +1,23 @@
+#ifndef STRATEGY_H
+#define STRATEGY_H
+
+class Strategy
+{
+private:
+    /* data */
+public:
+    Strategy(/* args */);
+    ~Strategy();
+};
+
+Strategy::Strategy(/* args */)
+{
+}
+
+Strategy::~Strategy()
+{
+}
+
+
+
+#endif // STRATEGY_H
