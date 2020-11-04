049e8b09faf7d4701defbaa1bb5653381b0da6e8
TheoMorkel <theo.jnr.morkel@gmail.com>
2020-11-04 09:41:10 +0200

    Added Correct tab spacing

--- carComposite/RaceCar.h
+++ carComposite/RaceCar.h
@@ -14,13 +14,13 @@ class RaceCar
         void notify();
         void attach();
         void detach();
-	void request();
-	virtual void lap();//the method to do a lap 
-	RaceCar* getChild(); // returns the child of the race car
-	virtual void degrade();//the degrade method 
-	virtual void addPart(RaceCar* car);// method to add parts to the race car 
+        void request();
+        virtual void lap();//the method to do a lap 
+        RaceCar* getChild(); // returns the child of the race car
+        virtual void degrade();//the degrade method 
+        virtual void addPart(RaceCar* car);// method to add parts to the race car 
     
-	//~ virtual void addPart(RaceCar* part) = 0;
+	    //~ virtual void addPart(RaceCar* part) = 0;
 
         //Strategy* getStrategy() const;
         //void setStrategy(Strategy* strat);
