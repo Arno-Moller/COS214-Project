382511877e4e992dcc3102b9b967670abf178c0d
TheoMorkel <theo.jnr.morkel@gmail.com>
2020-11-04 15:49:59 +0200

    Created Template Method

---
+++ Template/ConstructorsChampionship.cpp
@@ -0,0 +1,27 @@
+#include "ConstructorsChampionship.h"
+
+
+ConstructorsChampionship::ConstructorsChampionship(/* args */) 
+{
+    
+}
+
+ConstructorsChampionship::~ConstructorsChampionship() 
+{
+    
+}
+
+void ConstructorsChampionship::start() 
+{
+    
+}
+
+void ConstructorsChampionship::restart() 
+{
+    
+}
+
+void ConstructorsChampionship::stop() 
+{
+    
+}
