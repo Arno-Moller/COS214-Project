5d7b9a7d8ff94ed0d7d5454b06347a1094866a4a
LucaPrenzler <72298479+LucaPrenzler@users.noreply.github.com>
2020-11-07 12:10:31 +0200

    Doxygen Comments added in Prototype

--- Prototype/RacingTeam.h
+++ Prototype/RacingTeam.h
@@ -2,29 +2,83 @@
 #define RACINGTEAM_H
 
 #include "Team.h"
-//concrete prototype of the prototype design pattern 
+///concrete prototype of the prototype design pattern 
 class RacingTeam: public Team
 {
 private:
     
 public:
-    RacingTeam();//constructor
+    RacingTeam();///constructor
+
+    /**
+     * Constrcutor That takes in the tire compound of the car(soft , medium , hard)
+     * @param string tire compound 
+     */
     RacingTeam(string tireCompound);
+
+    /**
+     * Constrcutor That takes in the racing team 
+     * @param racingTeam  tire compound 
+     */
     RacingTeam(RacingTeam&);//constructor 
     
-    ~RacingTeam();//destructor 
+    ~RacingTeam();///destructor 
 
-    void buildCar(); // method to build a car 
-    void lap(); // method to do a lap 
-    void setTireCompound(string tireCompound);// set the tire compound
+    /**
+    * Method that builds the car 
+	* It adds all the elements to the car
+    */
+    void buildCar();
 
-    RaceCar* getCarOne(); // returns the first race car of the team 
-    RaceCar* getCarTwo(); // returns the second race car of the team 
-    void setCarOne(RaceCar* car1); // sets the first race car of the team 
-    void setCarTwo(RaceCar* car2); // sets the seconds race car of the team
 
-    Team* clone();//the clone method for the team 
+    /**
+    *  Method that allows both cars of the team to do laps 
+    */
+    void lap(); 
+
+    /**
+    *  method that sets the tire Compound(soft , hard , medium)
+	* @param tireCompound 
+    */
+    void setTireCompound(string tireCompound);
+    
+    /**
+    *  method that returns the first car of the team 
+	* @return RaceCar object of the first car of the team 
+    */
+    RaceCar* getCarOne(); 
+
+    /**
+    *  method that returns the second car of the team 
+	* @return RaceCar object of the second car of the team 
+    */
+    RaceCar* getCarTwo(); 
+
+    /**
+    *  method that sets the first car of the team 
+	* @return RaceCar object of the first car of the team 
+     */
+    void setCarOne(RaceCar* car1);
+
+    /**
+    * Abstract interface method that sets the second car of the team 
+	* @return RaceCar object of the first car of the team 
+    */
+    void setCarTwo(RaceCar* car2); 
+    
+    /**
+    * Abstract interface method that clones the team 
+	* @return Team object
+    */
+    Team* clone();
+
+    /**
+    * Abstract interface method that returns the teams points 
+	* @return int amount of the points the team has made
+    */
     int getTeamPoints();
+
+    
     void setTeamPoints();
     string getTeamName();
     void setTeamName(string name);
