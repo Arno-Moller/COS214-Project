00e811a366621c9b2b785e144f5e4b27a5299168
TheoMorkel <theo.jnr.morkel@gmail.com>
2020-11-09 12:19:08 +0200

    Added real driver names

--- Command/CreateTeamCommand.cpp
+++ Command/CreateTeamCommand.cpp
@@ -26,10 +26,10 @@ void CreateTeamCommand::execute()
     teams[0]->buildCar();
     teams[0]->setTeamName(names[0]);
     
-    string driver1 = names[0] + " Driver 1";
+    string driver1 = driverOneNames[0];
     teams[0]->getCarOne()->setName(driver1);
 
-    string driver2 = names[0] + " Driver 2";
+    string driver2 = driverTwoNames[0];
     teams[0]->getCarTwo()->setName(driver2);
     
     for (int i = 1; i < 10; i++)
@@ -37,10 +37,10 @@ void CreateTeamCommand::execute()
         teams[i] = teams[0]->clone();
         teams[i]->setTeamName(names[i]);
         
-        driver1 = names[i] + " Driver 1";
+        driver1 = driverOneNames[i];
         teams[i]->getCarOnePart()->setName(driver1);
 
-        driver2 = names[i] + " Driver 2";
+        driver2 = driverTwoNames[i];
         teams[i]->getCarTwoPart()->setName(driver2);
     }
     doesTheTeamsExist = true;
--- Command/CreateTeamCommand.h
+++ Command/CreateTeamCommand.h
@@ -23,6 +23,8 @@ private:
     Team** teams;
     TeamStateCaretaker** teamSaves;
     string names[10] = {"Mercedes AMG F1", "McLaren", "Ferrari", "Racing Point", "Red Bull Racing", "AlphaTauri", "Haas F1", "Renault", "Alfa Romeo Racing", "Williams Racing"};
+    string driverOneNames[10] = {"Lewis Hamilton", "Lando Norris", "Charles Leclerc", "Lance Stroll", "Max Verstappen", "Romain Grosjean", "Daniel Ricciardo", "Kimi Raikkonen", "George Russel"};
+    string driverTwoNames[10] = {"Valterri Bottas", "Carlos Sainz", "Sebastian Vettel", "Sergio Perez", "Alex Albon", "Kevin Magnussen", "Esteban Ocon", "Antonio Giovinazzi", "Nicholas Latifi"};
     bool doesTheTeamsExist = false;
 };
 
